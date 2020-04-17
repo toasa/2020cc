891e40a1388600396c29ccb85afbeb5864012ca5
atsu <atsushi.tohyama.160.333@gmail.com>
2020-04-17 23:43:02 +0900

    add: tokenize.

--- main.c
+++ main.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
 
+typedef enum {
+    TK_NUM,
+    TK_RESERVED,
+    TK_EOF,
+} TokenKind;
+
+typedef struct {
+    TokenKind tk;
+    int val;
+    char *str;
+} Token;
+
+Token new_token(TokenKind tk, int val, char *str) {
+    Token t = {tk, val, str};
+    return t;
+}
+
+Token tokens[100];
+
 int is_digit(char c) {
     return ('0' <= c && c <= '9');
 }
 
 int is_char(char c) {
     return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z');
+}
+
+int is_space(char c) {
+    return c == ' ';
 }
 
 int read_number(char **input) {
@@ -17,29 +40,70 @@ int read_number(char **input) {
     return num;
 }
 
-void gen_asm(char *input) {
-    char *c = input;
-    while (*c) {
-        if (is_digit(*c)) {
-            int num = read_number(&c);
-            printf("    mov rax, %d\n", num);
-        } else if (*c == '+') {
-            c++;
-            int num = read_number(&c);
-            printf("    add rax, %d\n", num);
-        } else if (*c == '-') {
-            c++;
-            int num = read_number(&c);
-            printf("    sub rax, %d\n", num);
+int equal_string(char *s1, char *s2) {
+    do {
+        if (*s1 != *s2) { return 0; }
+        s1++;
+        s2++;
+    } while (*s1 && *s2);
+    return 1;
+}
+
+void gen_asm() {
+    int i = 0;
+    Token t = tokens[i];
+    for (; t.tk != TK_EOF; t = tokens[i]) {
+        if (t.tk == TK_NUM) {
+            printf("    mov rax, %d\n", t.val);
+            i++;
+        } else if (t.tk == TK_RESERVED) {
+            if (equal_string("+", t.str)) {
+                i++;
+                Token num_token = tokens[i];
+                printf("    add rax, %d\n", num_token.val);
+            } else if (equal_string("-", t.str)) {
+                i++;
+                Token num_token = tokens[i];
+                printf("    sub rax, %d\n", num_token.val);
+            }
+            i++;
         }
     }
 }
 
+void skip(char **input) {
+    while (is_space(**input)) {
+        (*input)++;
+    }
+}
+
+void tokenize(char *input) {
+    int i = 0;
+    while (*input) {
+        skip(&input);
+        if (is_digit(*input)) {
+            int num = read_number(&input);
+            tokens[i] = new_token(TK_NUM, num, "");
+        } else if (*input == '+') {
+            input++;
+            tokens[i] = new_token(TK_RESERVED, 0, "+");
+        } else if (*input == '-') {
+            input++;
+            tokens[i] = new_token(TK_RESERVED, 0, "-");
+        } else {
+            break;
+        }
+        i++;
+    }
+    tokens[i] = new_token(TK_EOF, 0, "");
+}
+
 int main(int argc, char **argv) {
     char *input = argv[1];
+    tokenize(input);
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    gen_asm(input);
+    gen_asm();
     printf("    ret\n");
 }
--- test.sh
+++ test.sh
@@ -4,10 +4,9 @@ test() {
     expected=$1
     input=$2
 
-    clang main.c -o tmp
-    ./tmp ${input} > tmp.s
-    clang tmp.s -o main
-    ./main
+    ./main "$input" > tmp.s
+    clang tmp.s -o tmp
+    ./tmp
     result=$?
 
     if [ ${expected} -eq ${result} ]
@@ -24,4 +23,6 @@ test 30 30
 test 6 "1+2+3"
 test 20 "40-20"
 test 10 "10+40-10-30"
+test 3 "2 + 1"
+test 20 "  10 + 11- 1 "
 echo "OK"
